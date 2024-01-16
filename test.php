163d434caa9e164d0ea90bc09e36814329639d3f
unknown <safiyasara6363@gmail.com>
2024-01-16 09:16:55 +0530

    initial

---
+++ test.php
@@ -0,0 +1,20 @@
+
+<?php
+
+include __DIR__ . '/config.php';
+include __DIR__ . '/helpers/AppManager.php';
+
+require_once __DIR__ . '/models/User.php';
+
+$userModel = new User();
+
+if ($userModel->createUser("musab", "mypassword123", "operator", "musab@example.com")) {
+    echo "User created successfully!";
+} else {
+    echo "Failed to create user. May be user already exist!";
+}
+
+
+$users = $userModel->getAllActive();
+
+dd($users);
---
+++ views/auth/login.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once('./../../config.php');
+include __DIR__ . './../../helpers/AppManager.php';
+
+$sm = AppManager::getSM();
+$error = $sm->getAttribute("error");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tooth Care - Channeling</title>
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="<?= asset('assets/vendor/fonts/boxicons.css') ?>" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="<?= asset('assets/vendor/css/core.css') ?>" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?= asset('assets/vendor/css/theme-default.css') ?>" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?= asset('assets/css/demo.css') ?>" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="<?= asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') ?>" />
+
+    <link rel="stylesheet" href="<?= asset('assets/vendor/libs/apex-charts/apex-charts.css') ?>" />
+
+    <!-- Page -->
+    <link rel="stylesheet" href="<?= asset('assets/vendor/css/pages/page-auth.css') ?>" />
+
+    <!-- Helpers -->
+    <script src=" <?= asset('assets/vendor/js/helpers.js') ?>">
+    </script>
+</head>
+
+<body>
+    <!-- Content -->
+
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="#" class="app-brand-link gap-2">
+                                <span class="app-brand-text demo text-body fw-bolder" style=" text-transform: capitalize; "> Tooth Care</span>
+                            </a>
+                        </div>
+                        <!-- /Logo -->
+                        <form id="formAuthentication" class="mb-3" action="../../services/auth.php" method="post">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" autofocus />
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer"></span>
+                                </div>
+                            </div>
+                            <?php if (!empty($error)) : ?>
+                                <div class="alert alert-danger"><?= ($error ?? "") ?></div>
+                            <?php endif; ?>
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- /Register -->
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
